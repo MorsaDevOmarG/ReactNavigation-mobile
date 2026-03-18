cd52219e85d321aab570ee1145997197c4220d97
Omar <omar.garcia@morsa.com.mx>
2026-03-18 10:52:33 -0600

    Volver a la pantalla PREVIA

--- views/Nosotros.js
+++ views/Nosotros.js
@@ -1,8 +1,37 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-const Nosotros = () => {
-  return <Text>Nosotros</Text>;
+const Nosotros = props => {
+  console.log('props:', props);
+
+  const { navigation } = props;
+  console.log('navigation:', navigation);
+
+  const volver = () => {
+    // navigation.navigate('Inicio');
+
+    // Esto vuelve a Inicio pero crea una nueva pantalla, no vuelve a la anterior
+    // navigation.push('Inicio');
+
+    // Esto siempre vuelve a la anterior
+    navigation.goBack();
+  };
+
+  return (
+    <View style={styles.contenedor}>
+      <Text>Nosotros</Text>
+
+      <Button title="Volver" onPress={() => volver()} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Nosotros;
