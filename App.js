8a7685818939a05877a694339ef900cd80028f05
Omar <omar.garcia@morsa.com.mx>
2026-03-18 09:37:31 -0600

    Iniciando Proyecto

---
+++ App.js
@@ -0,0 +1,23 @@
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
+export const App = () => {
+  return (
+    <>
+      <Text>App</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
